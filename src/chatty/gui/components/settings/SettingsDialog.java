8b32f8c7bf7d8b046299c6c1fbb61691b9762f25
Kon <44456824+KonKode@users.noreply.github.com>
2020-03-30 16:03:23 -0400

    Revert "v0.12-b1, timezone restart warning"
    This reverts commit 8bc698105bf4dbcb8140e0257fb18956441ad0a4.

--- src/chatty/gui/components/settings/SettingsDialog.java
+++ src/chatty/gui/components/settings/SettingsDialog.java
@@ -61,7 +61,7 @@ public class SettingsDialog extends JDialog implements ActionListener {
             "bttvEmotes", "botNamesBTTV", "botNamesFFZ", "ffzEvent",
             "logPath", "logTimestamp", "logSplit", "logSubdirectories",
             "tabsPlacement", "tabsLayout", "logLockFiles",
-            "laf", "lafTheme", "lafFontScale", "language", "timezone"
+            "laf", "lafTheme", "lafFontScale", "language"
     ));
     
     private final Set<String> reconnectRequiredDef = new HashSet<>(Arrays.asList(
